1d0c8980e419a3090fcf6d5f33341f2872ee6124
Manu Saini <139550825+ManuSaini9084@users.noreply.github.com>
2024-04-17 17:43:50 +0530

    second commit

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -64,9 +64,7 @@ const Dashboard = () => {
                 </h5>
               </div>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Facilis, nam molestias. Eaque molestiae ipsam commodi neque.
-                Assumenda repellendus necessitatibus itaque.
+              This admin portal of our hospital management project serves as the control center for overseeing and managing various aspects of the healthcare facility.
               </p>
             </div>
           </div>
